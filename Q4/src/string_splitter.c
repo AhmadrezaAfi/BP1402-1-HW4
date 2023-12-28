d3d58c988fb1f0a3d21a550d1bf4d9433d0da38b
AhmadrezaAfi <87935331+AhmadrezaAfi@users.noreply.github.com>
2023-12-28 12:55:54 +0330

    add: files

--- Q4/src/string_splitter.c
+++ Q4/src/string_splitter.c
@@ -3,32 +3,9 @@
 #include <string.h>
 #include <ctype.h> 
 
-int main(){
-    int count;
-    char **result = splitString("Hello,World,Test", ',', &count);
-    if(count==3)
-        printf("1"); 
-    printf("%d", strcmp(result[0], "Hello"));
-//    EXPECT_STREQ(result[0], "Hello");
-//    EXPECT_STREQ(result[1], "World");
-//    EXPECT_STREQ(result[2], "Test");
-    freeSubstrings(result, count);
-}
 
 char** splitString(const char *input, char delimiter, int *substringCount) {
-    // Hint 1: Count delimiters to determine substring count.
-    // Hint 2: extract and allocate memory for each substring.
 
-
-    // Sample Case 1:
-    // Input: "One;Two;Three;Four", delimiter = ';', substringCount points to an integer
-    // Expected Output: ["One", "Two", "Three", "Four"]
-    // After execution, *substringCount should be updated to 4
-
-    // Sample Case 2:
-    // Input: "SingleString", delimiter = ',', substringCount points to an integer
-    // Expected Output: ["SingleString"]
-    // After execution, *substringCount should be updated to 1
     char **result = (char** )malloc(100*sizeof(char *));
 	char str1[1000], str2[100];
 	char* Pstr1, *pdelimiter;
@@ -61,11 +38,7 @@ char** splitString(const char *input, char delimiter, int *substringCount) {
 }
 
 void freeSubstrings(char **substrings, int count) {
-    // Hint: Free each substring, then free the substring array.
 
-    // Sample Case:
-    // Input: Array of substrings ["Hello", "World", "Test"], count = 3
-    // Operation: Frees memory for each substring and then the array itself
     for (int i=0; i<count; i++){
 		free(substrings[i]);
 	}
