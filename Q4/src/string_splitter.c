bc1a9124a7ce11845e2606907811fb362e07618f
AhmadrezaAfi <87935331+AhmadrezaAfi@users.noreply.github.com>
2023-12-28 23:31:32 +0330

    add files]

--- Q4/src/string_splitter.c
+++ Q4/src/string_splitter.c
@@ -4,52 +4,29 @@
 #include <ctype.h> 
 
 
-char** splitString(const char *input, char delimiter, int *substringCount) {
+char** splitstring(const char* input, char delimiter, int* substringCount ){
+	char del[2];
+	char* inp;
+	inp = strdup(input);
+	del[0]=delimiter; del[1]='\0';
+	char **result = (char** )malloc(Max*sizeof(char *));
+	char* token;
+	token = strtok(inp, del);
 
-    char **result = (char** )malloc(100*sizeof(char *));
-	char str1[1000], str2[100];
-	char* Pstr1, *pdelimiter;
-	Pstr1 = &str1[0];
-	strcpy(str1, input);
-	while (1){
-		int counter=0;
-		pdelimiter = strchr(str1, delimiter);
-		while(pdelimiter==Pstr1){
-			strcpy(str1, pdelimiter+1+counter);
-			pdelimiter = strchr(str1, delimiter);
-		}
-		while(pdelimiter!=NULL && str1[pdelimiter-Pstr1+1+counter]==delimiter){
-			counter++;
-		}
-		if(!pdelimiter)
-		    break;
-		if(*substringCount<100)
-		    result[*substringCount] = (char* )malloc(100*sizeof(char));
-		strncpy(str2, str1, (pdelimiter-Pstr1+counter));
-	    str2[pdelimiter-Pstr1] = '\0';
-	    strcpy(str1, pdelimiter+1+counter);
-		if(strlen(str2)<100&&*substringCount<100)
-	        strcpy(result[*substringCount], str2);
-	    (*substringCount)++;
-	}
-	if(*substringCount<100)
-	    result[*substringCount] = (char* )malloc(100*sizeof(char));
-	if(strlen(str2)<100&&*substringCount<100)
-	strcpy(result[*substringCount], str1);
+	while( token != NULL ) {
+        result[*substringCount] = (char* )malloc((token-input)*sizeof(char));
+        result[*substringCount] = token;
+        printf("%s\n",result[*substringCount] );
+        token = strtok(NULL, del);
         (*substringCount)++;
-	return result;
-
+   }
+   return result;
 }
 
-void freeSubstrings(char **substrings, int count) {
-
-    if(substrings){
-        for (int i=0; i<count; i++){
-		    free(substrings[i]);
-	    }
-	free(substrings);
+void freeSubstrings(char** substrings, int count){
+	for (int i=0; i<count; i++){
+		free(substrings[i]);
 	}
-
+	free(substrings);
 }
-
 
