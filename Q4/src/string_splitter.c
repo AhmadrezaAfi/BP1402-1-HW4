c21845370ef2a1686a2bf0da7012669b5cec7fef
AhmadrezaAfi <87935331+AhmadrezaAfi@users.noreply.github.com>
2024-01-06 17:28:22 +0330

    add files

--- Q4/src/string_splitter.c
+++ Q4/src/string_splitter.c
@@ -2,69 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h> 
-
-
+#define Max_Size 100
 
 char** splitString(const char* input, char delimiter, int* substringCount ){
-	*substringCount = 0;
-	int len = strlen(input);
-	int count=0;
-	char del[2];
-	char* inp;
-	inp = strdup(input);
-	del[0]=delimiter; del[1]='\0';
-	char **result = (char** )malloc(strlen(input)*sizeof(char *));
-	char* token;
-	token = strtok(inp, del);
-	if(count<len){
-	    if(input[count]==delimiter){
-	    	(*substringCount)++;
-	    	result[*substringCount]=strdup("");
-	    	count++;
-        	while(input[count]==delimiter){
-        		count++;
-        		if(count>=len){
-        			return result;
-        		    break;
-        		}
-			}
-		}
-	}
-	while( token != NULL ) {
-        result[*substringCount] = (char* )malloc((strlen(token)+1)*sizeof(char));
-        strcpy(result[*substringCount], token);
-		count+=(strlen(token)+1);
-		if(count>=len)
-		    break;
-//		printf("%s,,,,,%d\n", token, count);
-        if(input[count]==delimiter){
-        	(*substringCount)++;
-        	result[*substringCount]=strdup("");
-//        	printf("1\n");
-        	while(input[count]==delimiter){
-        		count++;
-        		if(count>=len){
-        			printf("1\n");
-        			(*substringCount)++;
-        			return result;
-        		    break;
-        		}
-			}
-		}
-        token = strtok(NULL, del);
+	*substringCount=0;
+    char** result = (char**)malloc(1*sizeof(char*));
+    char* pstr1, *pstr2;
+    char pstr3[Max_Size];
+    pstr1 = strdup(input);
+    do{
+//    	printf("%d\n", *substringCount);
+        pstr2 = strchr(pstr1, delimiter);
+//        printf("%s_____%s\n", pstr1, pstr2);
+        if(pstr2==NULL){
+            result[*substringCount] = (char*)malloc((strlen(pstr1)+1)*sizeof(char));
+            strcpy(result[*substringCount], pstr1);
+            result[*substringCount][strlen(pstr1)]='\0';
+            break;
+        }
+        strncpy(pstr3, pstr1, (size_t)(pstr2-pstr1));
+        pstr3[(size_t)(pstr2-pstr1)]='\0';
+        result = (char**)realloc(result, (*substringCount+2)*sizeof(char*));
+        result[*substringCount] = (char*)malloc((strlen(pstr3)+1)*sizeof(char));
+        strcpy(result[*substringCount], pstr3);
+        result[*substringCount][strlen(pstr3)]='\0';
+//        printf("%s\n%s\n%s\n%s\n\n", pstr1, pstr2, pstr3, result[*substringCount]);
         (*substringCount)++;
-   }
-
-   (*substringCount)++;
-   free(inp);
-   return result;
+        pstr1=pstr2+1;
+        if(*pstr1==delimiter){
+        	result = (char**)realloc(result, (*substringCount+2)*sizeof(char*));
+        	result[*substringCount]=strdup("");
+        	(*substringCount)++;
+		}
+	    while(*(pstr1++)==delimiter) continue;
+	    pstr1--;
+	}while(true);
+		
+//    printf("%s\n%s\n%s\n\n", result[0], result[1], result[2]);
+    return result;
 }
-
+    
 void freeSubstrings(char** substrings, int count){
 	for (int i=0; i<count; i++){
 		free(substrings[i]);
 	}
 	free(substrings);
 }
-
-
