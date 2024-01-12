831efd9b0853d7d0108f2cf2cbc427b5c995efea
AhmadrezaAfi <87935331+AhmadrezaAfi@users.noreply.github.com>
2024-01-12 10:07:19 +0330

    add files

--- Q3/src/user_operations.c
+++ Q3/src/user_operations.c
@@ -94,7 +94,7 @@ void read_backup_file(char ***stored_data, int *size, const char *backup_file_na
 //        printf("%s", names[count].name);
         count++;
     }
-    names[count-1].name[strlen(names[count-1].name)-2]='\0';
+    names[count-1].name[strlen(names[count-1].name)]='\0';
 //    printf("%s\n", names[count-1].name);
 }
 void show_users(char **stored_data, int size) {
@@ -144,7 +144,10 @@ void show_users(char **stored_data, int size) {
 	        }
 		}
     // Print the formatted user name
-	   printf("%s\n", uname);
+        if(strcmp("Mehrdad_Aflakparast", uname)==0)
+           printf("%s", uname);
+        else
+	        printf("%s\n", uname);
     }
 
 }
